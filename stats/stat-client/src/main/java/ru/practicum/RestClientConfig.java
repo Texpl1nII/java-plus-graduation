f0a7a4938668339f4ee21e64f527ab68883d8f38
Texpl1nII <Texpl1n@yandex.ru>
2026-03-26 18:44:31 +0500

    правки постман

--- stats/stat-client/src/main/java/ru/practicum/RestClientConfig.java
+++ stats/stat-client/src/main/java/ru/practicum/RestClientConfig.java
@@ -1,20 +1,15 @@
 package ru.practicum;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
-
-import java.util.Objects;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
-    public RestClient restClient(@Value("${stats-client.url}") String baseUrl) {
-        return RestClient.builder()
-                .baseUrl(Objects.requireNonNull(baseUrl))
-                .build();
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
     }
 }
 
--- stats/stat-client/src/main/java/ru/practicum/StatClientImpl.java
+++ stats/stat-client/src/main/java/ru/practicum/StatClientImpl.java
@@ -5,9 +5,12 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.ServiceInstance;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStatsDto;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -16,12 +19,28 @@ import java.util.List;
 @RequiredArgsConstructor
 public class StatClientImpl implements StatClient {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    private final RestClient restClient;
+    private final DiscoveryClient discoveryClient;
+    private final RestClient.Builder restClientBuilder;
+    private static final String STATS_SERVICE_ID = "stats-server";
+
+    private ServiceInstance getInstance() {
+        List<ServiceInstance> instances = discoveryClient.getInstances(STATS_SERVICE_ID);
+        if (instances == null || instances.isEmpty()) {
+            throw new RuntimeException("Сервис статистики не найден в Eureka");
+        }
+        return instances.get(0);
+    }
+
+    private URI makeUri(String path) {
+        ServiceInstance instance = getInstance();
+        return URI.create("http://" + instance.getHost() + ":" + instance.getPort() + path);
+    }
 
     @Override
     public void hit(@NonNull EndpointHitDto paramHitDto) {
+        RestClient restClient = restClientBuilder.build();
         restClient.post()
-                .uri("/hit")
+                .uri(makeUri("/hit"))
                 .body(paramHitDto)
                 .retrieve()
                 .toBodilessEntity();
@@ -29,26 +48,19 @@ public class StatClientImpl implements StatClient {
 
     @Override
     public List<ViewStatsDto> getStat(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+        RestClient restClient = restClientBuilder.build();
+
+        URI uri = makeUri("/stats");
 
         return restClient.get()
-                .uri(uriBuilder -> {
-                    var builder = uriBuilder
-                            .path("/stats")
-                            .queryParam("start", start.format(FORMATTER))
-                            .queryParam("end", end.format(FORMATTER));
-
-                    if (uris != null && !uris.isEmpty()) {
-                        builder.queryParam("uris", uris);
-                    }
-
-                    if (unique != null) {
-                        builder.queryParam("unique", unique);
-                    }
-
-                    return builder.build();
-                })
+                .uri(uriBuilder -> uriBuilder
+                        .path(uri.getPath())
+                        .queryParam("start", start.format(FORMATTER))
+                        .queryParam("end", end.format(FORMATTER))
+                        .queryParam("uris", uris != null && !uris.isEmpty() ? uris : null)
+                        .queryParam("unique", unique)
+                        .build())
                 .retrieve()
                 .body(new ParameterizedTypeReference<List<ViewStatsDto>>() {});
     }
 }
-
