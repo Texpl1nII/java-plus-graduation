ddc31aa47a628ffc47e34dd87d71e1ea6291e563
Texpl1nII <Texpl1n@yandex.ru>
2026-03-31 18:32:11 +0500

    правки

--- core/compilation-service/src/main/java/ru/practicum/compilation/model/Compilation.java
+++ core/compilation-service/src/main/java/ru/practicum/compilation/model/Compilation.java
@@ -1,9 +1,9 @@
 package ru.practicum.compilation.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import jakarta.persistence.*;
 import org.springframework.data.annotation.Id;
 
 import java.util.HashSet;
