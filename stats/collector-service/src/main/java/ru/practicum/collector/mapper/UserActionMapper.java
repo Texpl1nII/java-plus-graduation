b7a38b32074e80ff990dccce4f0687342aef245f
Texpl1nII <Texpl1n@yandex.ru>
2026-04-09 20:50:04 +0500

    правки сборки

--- stats/collector-service/src/main/java/ru/practicum/collector/mapper/UserActionMapper.java
+++ stats/collector-service/src/main/java/ru/practicum/collector/mapper/UserActionMapper.java
@@ -6,18 +6,23 @@ import ru.practicum.collector.model.ActionType;
 import ru.practicum.ewm.stats.avro.UserActionAvro;
 import ru.practicum.stats.proto.UserActionMessage;
 
+import java.time.Instant;
+
 @UtilityClass
 public class UserActionMapper {
 
     public static UserActionAvro toAvro(UserActionMessage protoMessage) {
         ActionType actionType = ActionType.fromProto(protoMessage.getActionType());
 
+        // Правильная конвертация Protobuf Timestamp → milliseconds
+        long timestampMillis = protoMessage.getTimestamp().getSeconds() * 1000 +
+                protoMessage.getTimestamp().getNanos() / 1_000_000;
+
         return UserActionAvro.newBuilder()
                 .setUserId(protoMessage.getUserId())
                 .setEventId(protoMessage.getEventId())
                 .setActionType(ru.practicum.ewm.stats.avro.ActionTypeAvro.valueOf(actionType.name()))
-                .setTimestamp(protoMessage.getTimestamp().getSeconds() * 1000 +
-                        protoMessage.getTimestamp().getNanos() / 1_000_000)
+                .setTimestamp(timestampMillis)  // ← long, а не Instant
                 .build();
     }
 }
