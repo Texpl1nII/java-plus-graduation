b1d78f1c71a38ccb958d3aace74d4e267a9ac876
Texpl1nII <Texpl1n@yandex.ru>
2026-04-11 21:35:12 +0500

    правки

--- stats/collector-service/src/main/java/ru/practicum/collector/kafka/UserActionProducer.java
+++ stats/collector-service/src/main/java/ru/practicum/collector/kafka/UserActionProducer.java
@@ -2,31 +2,23 @@ package ru.practicum.collector.kafka;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecordBase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import ru.practicum.ewm.stats.avro.UserActionAvro;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class UserActionProducer {
 
-    private final KafkaTemplate<String, UserActionAvro> kafkaTemplate;
+    private final KafkaTemplate<String, SpecificRecordBase> kafkaTemplate;
 
     @Value("${kafka.topics.user-actions}")
-    private String topic;
+    private String userActionsTopic;
 
-    public void send(UserActionAvro userAction) {
-        log.info("Sending message to topic {}: {}", topic, userAction);
-        kafkaTemplate.send(topic, String.valueOf(userAction.getUserId()), userAction)
-                .whenComplete((result, ex) -> {
-                    if (ex == null) {
-                        log.debug("Message sent successfully to offset: {}",
-                                result.getRecordMetadata().offset());
-                    } else {
-                        log.error("Failed to send message", ex);
-                    }
-                });
+    public void send(SpecificRecordBase message) {
+        log.info("Sending message to topic {}: {}", userActionsTopic, message);
+        kafkaTemplate.send(userActionsTopic, message);
     }
 }
