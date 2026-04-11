ed0ad3a911695ac87364e2bf201693dab9a40fac
Texpl1nII <Texpl1n@yandex.ru>
2026-04-11 20:51:42 +0500

    правки

--- stats/collector-service/src/main/java/ru/practicum/collector/grpc/UserActionControllerImpl.java
+++ stats/collector-service/src/main/java/ru/practicum/collector/grpc/UserActionControllerImpl.java
@@ -1,15 +1,16 @@
 package ru.practicum.collector.grpc;
 
-import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 import ru.practicum.collector.kafka.UserActionProducer;
-import ru.practicum.collector.mapper.UserActionMapper;
-import ru.practicum.stats.proto.EmptyResponse;
-import ru.practicum.stats.proto.UserActionControllerGrpc;
-import ru.practicum.stats.proto.UserActionMessage;
+import ru.practicum.ewm.stats.avro.UserActionAvro;
+import ru.practicum.ewm.stats.proto.UserActionControllerGrpc;
+import ru.practicum.ewm.stats.proto.UserActionRequest;
+import ru.practicum.ewm.stats.proto.Empty;
+
+import java.time.Instant;
 
 @Slf4j
 @GrpcService
@@ -19,27 +20,47 @@ public class UserActionControllerImpl extends UserActionControllerGrpc.UserActio
     private final UserActionProducer userActionProducer;
 
     @Override
-    public void collectUserAction(UserActionMessage request, StreamObserver<EmptyResponse> responseObserver) {
-        log.info("Received user action: userId={}, eventId={}, actionType={}, timestamp={}",
-                request.getUserId(),
-                request.getEventId(),
-                request.getActionType(),
-                request.getTimestamp());
-
+    public void collectUserAction(UserActionRequest request, StreamObserver<Empty> responseObserver) {
         try {
-            // Конвертируем Protobuf → Avro и отправляем в Kafka
-            var avroMessage = UserActionMapper.toAvro(request);
-            userActionProducer.send(avroMessage);
+            log.info("Received user action: userId={}, eventId={}, actionType={}, timestamp={}",
+                    request.getUserId(),
+                    request.getEventId(),
+                    request.getActionType(),
+                    request.getTimestamp());
 
-            log.info("Successfully sent to Kafka: {}", avroMessage);
+            UserActionAvro userActionAvro = UserActionAvro.newBuilder()
+                    .setUserId(request.getUserId())
+                    .setEventId(request.getEventId())
+                    .setActionType(mapActionType(request.getActionType()))
+                    .setTimestamp(Instant.ofEpochSecond(
+                            request.getTimestamp().getSeconds(),
+                            request.getTimestamp().getNanos()
+                    ))
+                    .build();
 
-            // Отправляем успешный ответ
-            responseObserver.onNext(EmptyResponse.newBuilder().build());
+            userActionProducer.send(userActionAvro);
+
+            responseObserver.onNext(Empty.newBuilder().build());
             responseObserver.onCompleted();
 
         } catch (Exception e) {
             log.error("Error processing user action", e);
             responseObserver.onError(e);
+        }
+    }
+
+    private ru.practicum.ewm.stats.avro.ActionTypeAvro mapActionType(
+            ru.practicum.ewm.stats.proto.ActionTypeProto actionTypeProto) {
+
+        switch (actionTypeProto) {
+            case ACTION_VIEW:
+                return ru.practicum.ewm.stats.avro.ActionTypeAvro.VIEW;
+            case ACTION_REGISTER:
+                return ru.practicum.ewm.stats.avro.ActionTypeAvro.REGISTER;
+            case ACTION_LIKE:
+                return ru.practicum.ewm.stats.avro.ActionTypeAvro.LIKE;
+            default:
+                throw new IllegalArgumentException("Unknown action type: " + actionTypeProto);
         }
     }
 }
--- stats/collector-service/src/main/java/ru/practicum/collector/kafka/UserActionProducer.java
+++ stats/collector-service/src/main/java/ru/practicum/collector/kafka/UserActionProducer.java
@@ -2,23 +2,31 @@ package ru.practicum.collector.kafka;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.avro.specific.SpecificRecordBase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import ru.practicum.ewm.stats.avro.UserActionAvro;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class UserActionProducer {
 
-    private final KafkaTemplate<String, SpecificRecordBase> kafkaTemplate;
+    private final KafkaTemplate<String, UserActionAvro> kafkaTemplate;
 
     @Value("${kafka.topics.user-actions}")
-    private String userActionsTopic;
+    private String topic;
 
-    public void send(SpecificRecordBase message) {
-        log.info("Sending message to topic {}: {}", userActionsTopic, message);
-        kafkaTemplate.send(userActionsTopic, message);
+    public void send(UserActionAvro userAction) {
+        log.info("Sending message to topic {}: {}", topic, userAction);
+        kafkaTemplate.send(topic, String.valueOf(userAction.getUserId()), userAction)
+                .whenComplete((result, ex) -> {
+                    if (ex == null) {
+                        log.debug("Message sent successfully to offset: {}",
+                                result.getRecordMetadata().offset());
+                    } else {
+                        log.error("Failed to send message", ex);
+                    }
+                });
     }
 }
