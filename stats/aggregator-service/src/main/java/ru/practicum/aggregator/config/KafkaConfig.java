88d089facc1456c904e5c70bc9404d30162f9dac
Texpl1nII <Texpl1n@yandex.ru>
2026-04-13 00:18:55 +0500

    правки

--- stats/aggregator-service/src/main/java/ru/practicum/aggregator/config/KafkaConfig.java
+++ stats/aggregator-service/src/main/java/ru/practicum/aggregator/config/KafkaConfig.java
@@ -33,6 +33,11 @@ public class KafkaConfig {
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "aggregator-group");
         configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         configProps.put("specific.avro.value.class", "ru.practicum.ewm.stats.avro.UserActionAvro");
+        // Consumer таймауты
+        configProps.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 500);
+        configProps.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, 5000);
+        configProps.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 30000);
+        configProps.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, 3000);
 
         return new DefaultKafkaConsumerFactory<>(configProps);
     }
@@ -53,6 +58,15 @@ public class KafkaConfig {
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
+
+        // Увеличенные таймауты для producer
+        configProps.put(ProducerConfig.ACKS_CONFIG, "all");
+        configProps.put(ProducerConfig.RETRIES_CONFIG, 10);
+        configProps.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, 1000);
+        configProps.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 120000);
+        configProps.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 120000);
+        configProps.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, 120000);
+        configProps.put(ProducerConfig.CLIENT_ID_CONFIG, "aggregator-producer");
 
         return new DefaultKafkaProducerFactory<>(configProps);
     }
