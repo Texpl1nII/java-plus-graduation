0682cd3c7d0b5a7d16e27a6db38dbd0b3cebdae6
Texpl1nII <Texpl1n@yandex.ru>
2026-04-12 22:31:48 +0500

    правки

--- stats/analyzer-service/src/main/java/ru/practicum/analyzer/config/KafkaConfig.java
+++ stats/analyzer-service/src/main/java/ru/practicum/analyzer/config/KafkaConfig.java
@@ -2,14 +2,14 @@ package ru.practicum.analyzer.config;
 
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import ru.practicum.kafka.serializer.GeneralAvroSerializer;
+import ru.practicum.kafka.serializer.GeneralAvroDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,21 +21,21 @@ public class KafkaConfig {
     private String bootstrapServers;
 
     @Bean
-    public ConsumerFactory<Long, SpecificRecordBase> consumerFactory() {
+    public ConsumerFactory<String, SpecificRecordBase> consumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
-        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GeneralAvroDeserializer.class);  // ← ИСПРАВЛЕНО!
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "analyzer-group");
         configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        configProps.put("specific.avro.value.class", "ru.practicum.ewm.stats.avro.UserActionAvro");
+        configProps.put("specific.avro.value.class", "ru.practicum.ewm.stats.avro.EventSimilarityAvro");  // ← ИСПРАВЛЕНО!
 
         return new DefaultKafkaConsumerFactory<>(configProps);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<Long, SpecificRecordBase> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<Long, SpecificRecordBase> factory =
+    public ConcurrentKafkaListenerContainerFactory<String, SpecificRecordBase> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, SpecificRecordBase> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
